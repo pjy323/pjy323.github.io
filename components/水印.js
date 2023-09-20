216a1f5b63ae54a9c1df616eeac023295b6ed897
神经蛙 <111210953+pjy323@users.noreply.github.com>
2023-09-20 23:49:45 +0800

    Update 水印.js

--- components/水印.js
+++ components/水印.js
@@ -1,1 +1,15 @@
-
+export edefault {
+  template: `
+  <var-watermark 
+    content="神经蛙" 
+    :opacity="0.2" 
+    :fullscreen="true" 
+    :offset-x="10"
+    :offset-y="10"
+  >
+    <div class="watermark-wrapper">
+      <var-button type="primary" @click="toggle">切换</var-button>
+    </div>
+  </var-watermark>
+  `
+}
