a8d261ff4578cbff566dcae6e0a894983e896e0f
神经蛙 <111210953+pjy323@users.noreply.github.com>
2023-09-20 22:11:04 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import 导航栏 from './components/导航栏.js'
+import 结果 from './components/结果.js'
+const App = Vue.createApp({
+    components: {
+        'V-结果': 结果
+    },
+    template: `
+    <V-导航栏/>
+    <V-结果/>
+    `
+})
+App.use(Varlet)
+App.component(
+    'V-导航栏', 导航栏
+)
+App.mount('#App')
