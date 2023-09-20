cf85ec77ce2ed60d279fd2999a1cc985ba37d0a6
神经蛙 <111210953+pjy323@users.noreply.github.com>
2023-09-20 23:43:19 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import 导航栏 from './components/导航栏.js'
 import 结果 from './components/结果.js'
+import 水印 from './components/水印.js'
 const App = Vue.createApp({
     components: {
-        'V-结果': 结果
+        'V-结果': 结果,
+        'V-水印': 水印
     },
     template: `
     <V-导航栏/>
     <br/>
     <V-结果/>
+    <br/>
+    <V-水印/>
     `
 })
 App.use(Varlet)
